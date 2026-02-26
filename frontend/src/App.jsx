fd6b085c03c8dab7375b8b862062d62bcecdd585
chandanchaurasiya55 <chandanchaurasiya55042@gmail.com>
2026-02-26 16:27:22 +0530

    Frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ import Pricing from "./components/Pricing.jsx";
 import Studio from "./components/Studio.jsx";
 import Footer from "./components/Footer.jsx";
 import ServiceDetail from "./components/ServiceDetail.jsx";
-import Question from "./components/question.jsx";
+import Question from "./components/Question.jsx";
 
 // Auth Pages
 import UserLogin from "./Auth/UserLogin.jsx";
